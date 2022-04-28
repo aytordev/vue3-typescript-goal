0d14d16157b2c3dd5097f17eb54047d694369652
Aytor Vicente Martínez <avicentemart@outlook.com>
2022-04-28 21:11:30 +0200

    Implements alias & template static check

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import vue from "@vitejs/plugin-vue";
+import { resolve } from 'path';
 import { defineConfig } from "vite";
 import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   plugins: [vue(), eslintPlugin()],
 });
