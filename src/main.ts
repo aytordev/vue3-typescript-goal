211b24aa03c56bd1ab3a1edf7bad46d6149b1a00
Aytor Vicente Martínez <avicentemart@outlook.com>
2022-04-28 21:54:09 +0200

    Implements pinia as project store

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import App from '@/App.vue';
+import '@/assets/styles/index.scss';
+import store from '@/store';
+import { createApp } from 'vue';
 
-createApp(App).mount("#app");
+const application = createApp(App);
+
+application.use(store);
+application.mount('#app');
