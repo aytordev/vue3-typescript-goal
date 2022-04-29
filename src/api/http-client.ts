61d8ef41eef0d05c6627972293bf8a90ea6bec49
Aytor Vicente Martínez <avicentemart@outlook.com>
2022-04-29 17:36:35 +0200

    removing interceptors

--- src/api/http-client.ts
+++ src/api/http-client.ts
@@ -1,19 +1,5 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
-interface HttpResponse<T = unknown> {
-    status: number;
-    msg: string;
-    code: number;
-    data: T;
-}
-
-enum StatusCode {
-    Unauthorized = 401,
-    Forbidden = 403,
-    TooManyRequests = 429,
-    InternalServerError = 500,
-}
-
 const headers: Readonly<Record<string, string>> = {
     Accept: 'application/json',
     'Content-Type': 'application/json; charset=utf-8',
@@ -23,34 +9,6 @@ const http = axios.create({
     baseURL: 'http://localhost:3000',
     headers,
 });
-
-http.interceptors.response.use(
-    (response: AxiosResponse<HttpResponse>) => response,
-    (error) => {
-        const { status } = error;
-
-        switch (status) {
-            case StatusCode.InternalServerError: {
-                // Handle InternalServerError
-                break;
-            }
-            case StatusCode.Forbidden: {
-                // Handle Forbidden
-                break;
-            }
-            case StatusCode.Unauthorized: {
-                // Handle Unauthorized
-                break;
-            }
-            case StatusCode.TooManyRequests: {
-                // Handle TooManyRequests
-                break;
-            }
-        }
-
-        return Promise.reject(error);
-    }
-);
 
 const request = <T = any, R = AxiosResponse<T>>(
     config: AxiosRequestConfig
